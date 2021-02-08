dc2e29f5a83388c53b18672c6b999335acdf7656
martinurueta <71405406+martinurueta@users.noreply.github.com>
2021-02-07 16:01:29 -0800

    Delete warmup.py

--- warmup.py
+++
@@ -1,7 +0,0 @@
-from logic import TruthTable
-myTable = TruthTable(['a','b'], ['a and -b'])
-myTable.display()
-myTable.latex()
-myTable2 = TruthTable(['a','b','c'], ['(a and b) or -c'])
-myTable2.display()
-myTable2.latex()
